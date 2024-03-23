65b8341c4fa1f5f96f148118fa9bc9ad88991d24
Jisun696 <istiakkashem35@gmail.com>
2024-03-23 23:27:35 +0600

    added font family general sans from font share

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,17 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <h1 className="bg-[#09090B] text-white text-8xl p-80">Hail Satan</h1>,
+    element: (
+      <div className="p-20">
+        <a
+          href="#_"
+          className="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-lg shadow-xl group hover:ring-1 hover:ring-[#9873FF]">
+          <span className="absolute inset-0 w-full h-full bg-gradient-to-r from-[#7E90FE]  to-[#9873FF]"></span>
+          <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-[#7f52fc] rounded-full opacity-30 group-hover:rotate-90 ease"></span>
+          <span className="relative text-white">Button Text</span>
+        </a>
+      </div>
+    ),
   },
 ]);
 
